414e0d424d84f0ea7e7413928fd55dc4abe8f5f9
Youssef Atta'i <youssefgalalnazem@gmail.com>
2024-04-27 18:15:56 +0300

    Write setup script

---
+++ setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+VERSION = "0.0.1"
+
+NAME = "prisoners-dilemma-sdk"
+
+INSTALL_REQUIRES = []
+
+
+setuptools.setup(
+    name=NAME,
+    version=VERSION,
+    description="",
+    url=f"https://github.com/youssef-attai/{NAME}",
+    project_urls={
+        "Source Code": f"https://github.com/youssef-attai/{NAME}"
+    },
+    author="Youssef Atta'i",
+    author_email="youssefjattai@gmail.com",
+    license="MIT",
+    classifiers=[
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3.10",
+    ],
+    # Snowpark requires Python 3.8
+    python_requires=">=3.8",
+    # Requirements
+    install_requires=INSTALL_REQUIRES,
+    packages=["prisoners_dilemma_sdk"],
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+)
