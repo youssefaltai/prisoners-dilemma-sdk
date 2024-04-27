2e4857049e732a40bc9e7bca4fc7caa9151f6861
Youssef Atta'i <youssefgalalnazem@gmail.com>
2024-04-27 20:04:12 +0300

    Document more

--- prisoners_dilemma_sdk/core.py
+++ prisoners_dilemma_sdk/core.py
@@ -11,22 +11,31 @@ class GameState:
     Represents the state of the game.
     """
 
+    round_number: int
+    """The current round number."""
+
+    my_history: List[bool]
+    """The history of my decisions."""
+
+    opponent_history: List[bool]
+    """The history of the opponent's decisions."""
+
     def __init__(
         self,
         round_number: int,
-        player1_history: List[bool],
-        player2_history: List[bool]
+        my_history: List[bool],
+        opponent_history: List[bool],
     ):
         """
         Initialize the game state.
 
         :param round_number: The current round number.
-        :param player1_history: History of decisions made by player 1.
-        :param player2_history: History of decisions made by player 2.
+        :param my_history: The history of my decisions.
+        :param opponent_history: The history of the opponent's decisions.
         """
         self.round_number = round_number
-        self.player1_history = player1_history
-        self.player2_history = player2_history
+        self.my_history = my_history
+        self.opponent_history = opponent_history
 
 
 class PrisonersDilemmaStrategy(ABC):
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import setuptools
 
-VERSION = "0.0.6"
+VERSION = "0.0.7"
 
 NAME = "prisoners-dilemma-sdk"
 
